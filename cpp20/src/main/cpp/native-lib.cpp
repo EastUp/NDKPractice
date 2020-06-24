a3acaf37b7588198ff22a47875717b867d42a27e
GitHub EastUp <GitHub 631820188@qq.com>
2020-06-24 13:49:24 +0800

    实现 Native 层的 ArrayList

---
+++ cpp20/src/main/cpp/native-lib.cpp
@@ -0,0 +1,27 @@
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+
+#include "ArrayList.hpp"
+
+extern "C"
+
+JNIEXPORT jstring JNICALL Java_com_east_cpp20_MainActivity_stringFromJNI
+(JNIEnv* env,jobject jobj){
+    ArrayList<int> *list = new ArrayList<int>();
+
+    for(int i=0 ; i < 100; ++i){
+        list->add(i);
+    }
+
+    list->remove(1);
+    __android_log_print(ANDROID_LOG_ERROR,"TAG","size = %d",list->size());
+
+    for(int i =0 ; i< list->size(); ++i){
+        __android_log_print(ANDROID_LOG_ERROR,"TAG","i = %d",list->get(i));
+    }
+
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
---
+++ cpp20/src/main/java/com/east/cpp20/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.east.cpp20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv.text = stringFromJNI()
+    }
+
+
+    external fun stringFromJNI():String
+    companion object{
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+
+}
